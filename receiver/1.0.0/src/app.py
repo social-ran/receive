9f77ff69db66b390743575e20a26106a0f173d74
social-ran <68770158+social-ran@users.noreply.github.com>
2020-08-04 08:50:56 +0800

    Update app.py

--- receiver/1.0.0/src/app.py
+++ receiver/1.0.0/src/app.py
@@ -4,7 +4,7 @@ from walkoff_app_sdk.app_base import AppBase
 
 class receiver(AppBase):
     __version__ = "1.0.0"
-    app_name = "calculater"  # this needs to match "name" in api.yaml
+    app_name = "receiver"  # this needs to match "name" in api.yaml
     def __init__(self, redis, logger, console_logger=None):
         super().__init__(redis, logger, console_logger)
 
